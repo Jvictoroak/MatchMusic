3167c2b7ac38469d4c970812ea46e89d2aeaa555
João Victor Carvalho <joaovictor.crvl@gmail.com>
2024-09-29 00:24:47 -0300

    main.js update

--- public/main.js
+++ public/main.js
@@ -7,7 +7,7 @@ import { getRecommendations, recommendationsList } from './getRecommendations.js
 import { addTrackToPlaylist, searchMusic, selectedTracksIds, showSearch } from './searchMusic.js';
 import { criarIframe } from './base.js';
 
-
+console.log(import.meta.env)
 const clientId =import.meta.env.VITE_CLIENT_ID // Coloque aqui o Client ID
 
 if (!clientId) {
